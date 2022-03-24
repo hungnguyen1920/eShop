ec38d9b653c02744caf1afd1324eacd85d9bb558
DESKTOP-51C87GA\HUNG NGUYEN <hung1920.nguyen@gmail.com>
2022-03-25 00:55:18 +0700

    add Entities

---
+++ eShop.Data/EF/eShopDbContext.cs
@@ -0,0 +1,49 @@
+﻿using eShop.Data.Configurations;
+using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.EF
+{
+    public class eShopDbContext : DbContext
+    {
+        public eShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder moderBuilder)
+        {
+            moderBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            moderBuilder.ApplyConfiguration(new ProductConfiguration());
+            moderBuilder.ApplyConfiguration(new CategoryConfiguration());
+            moderBuilder.ApplyConfiguration(new OrderConfiguration());
+            moderBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            moderBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            moderBuilder.ApplyConfiguration(new ContactConfiguration());
+            moderBuilder.ApplyConfiguration(new LanguageConfiguration());
+            moderBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            moderBuilder.ApplyConfiguration(new PromotionConfiguration());
+            moderBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            moderBuilder.ApplyConfiguration(new ProductCategoryMapConfiguration());
+
+            //base.OnModelCreating(moderBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<ProductCategoryMap> ProductCategoryMaps { get; set; }
+    }
+}
---
+++ eShop.Data/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual ICollection<ProductCategoryMap> ProductCategoryMaps { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+        public virtual ICollection<ProductTranslation> ProductTranslations { get; set; }
+    }
+}
